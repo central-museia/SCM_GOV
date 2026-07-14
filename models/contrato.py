f25c87b00e860d795dc73c04ba99cc4658392196
central-museia <agents.museia@gmail.com>
2026-07-13 22:48:39 -0300

    Update contrato.py

--- models/contrato.py
+++ models/contrato.py
@@ -1,1 +1,29 @@
+"""
+Modelo de Contrato
 
+Autor: SCM_GOV
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Contrato:
+
+    numero: str = ""
+
+    objeto: str = ""
+
+    orgao: str = ""
+
+    contratado: str = ""
+
+    cnpj: str = ""
+
+    valor: float = 0.0
+
+    inicio: str = ""
+
+    fim: str = ""
+
+    situacao: str = ""
