066b177017f60f78691cd1eeac4771b03bf49710
central-museia <agents.museia@gmail.com>
2026-07-13 22:49:18 -0300

    Update estatistica.py

--- models/estatistica.py
+++ models/estatistica.py
@@ -1,1 +1,19 @@
+"""
+Modelo de Estatísticas
 
+Autor: SCM_GOV
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Estatistica:
+
+    total: int = 0
+
+    oportunidades: int = 0
+
+    canceladas: int = 0
+
+    score_medio: float = 0.0
