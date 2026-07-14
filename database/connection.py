ec2385eb73282f1f85be99b7bbe358fe0a41d056
central-museia <agents.museia@gmail.com>
2026-07-14 11:01:07 -0300

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -1,20 +1,11 @@
-"""
-Conexão SQLite
-
-Autor: SCM_GOV
-"""
-
 import sqlite3
 from pathlib import Path
 
-
 DATABASE = Path(__file__).parent / "scmgov.db"
-
 
 def conectar():
 
     conn = sqlite3.connect(DATABASE)
-
     conn.row_factory = sqlite3.Row
 
     return conn
