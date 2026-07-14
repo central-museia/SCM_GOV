fde7a3febafc8294237bddb3e686ac310548f318
central-museia <agents.museia@gmail.com>
2026-07-13 22:48:51 -0300

    Update orgao.py

--- models/orgao.py
+++ models/orgao.py
@@ -1,1 +1,25 @@
+"""
+Modelo de Órgão Público
 
+Autor: SCM_GOV
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Orgao:
+
+    cnpj: str = ""
+
+    nome: str = ""
+
+    esfera: str = ""
+
+    poder: str = ""
+
+    municipio: str = ""
+
+    uf: str = ""
+
+    codigo_unidade: str = ""
