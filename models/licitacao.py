0343daf833a129b7bb0503c82d65c69860b74272
central-museia <agents.museia@gmail.com>
2026-07-13 22:48:14 -0300

    Update licitacao.py

--- models/licitacao.py
+++ models/licitacao.py
@@ -1,1 +1,33 @@
+"""
+Modelo de Licitação
 
+Autor: SCM_GOV
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Licitacao:
+
+    numero: str = ""
+
+    objeto: str = ""
+
+    orgao: str = ""
+
+    municipio: str = ""
+
+    uf: str = ""
+
+    modalidade: str = ""
+
+    valor: float = 0.0
+
+    abertura: str = ""
+
+    encerramento: str = ""
+
+    situacao: str = ""
+
+    link: str = ""
