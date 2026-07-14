aa4f51dc600f243d3caa75519e96ba0244593c01
central-museia <agents.museia@gmail.com>
2026-07-13 22:49:29 -0300

    Update usuario.py

--- models/usuario.py
+++ models/usuario.py
@@ -1,1 +1,21 @@
+"""
+Modelo de Usuário
 
+Autor: SCM_GOV
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Usuario:
+
+    id: int = 0
+
+    nome: str = ""
+
+    email: str = ""
+
+    perfil: str = "Administrador"
+
+    ativo: bool = True
