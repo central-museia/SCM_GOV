6e228ec4bec7ea505ef8e2f564bdfa265e779e09
central-museia <agents.museia@gmail.com>
2026-07-14 11:01:19 -0300

    Update schema.py

--- database/schema.py
+++ database/schema.py
@@ -1,12 +1,30 @@
+"""
+Schema do banco
+
+Autor: SCM Engenharia
+"""
+
 from database.connection import conectar
 
 
 def criar_banco():
+
     conn = conectar()
 
     cursor = conn.cursor()
 
-    # criação das tabelas
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS configuracoes (
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        chave TEXT,
+
+        valor TEXT
+
+    )
+    """)
 
     conn.commit()
+
     conn.close()
