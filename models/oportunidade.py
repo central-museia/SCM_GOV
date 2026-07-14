53e1ec0d853be74205807728625bd09090a4ec8d
central-museia <agents.museia@gmail.com>
2026-07-13 22:49:04 -0300

    Update oportunidade.py

--- models/oportunidade.py
+++ models/oportunidade.py
@@ -1,1 +1,19 @@
+"""
+Modelo de Oportunidade
 
+Autor: SCM_GOV
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Oportunidade:
+
+    score: int = 0
+
+    classificacao: str = ""
+
+    oportunidade: bool = False
+
+    motivo: str = ""
