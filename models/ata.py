13104cd43ae3bc2c0d4124089dcef205cce90ba4
central-museia <agents.museia@gmail.com>
2026-07-13 22:48:28 -0300

    Add Ata class for price registration model

--- models/ata.py
+++ models/ata.py
@@ -1,1 +1,33 @@
+"""
+Modelo de Ata de Registro de Preço
 
+Autor: SCM_GOV
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Ata:
+
+    numero: str = ""
+
+    ano: int = 0
+
+    objeto: str = ""
+
+    orgao: str = ""
+
+    unidade: str = ""
+
+    vigencia_inicio: str = ""
+
+    vigencia_fim: str = ""
+
+    assinatura: str = ""
+
+    publicacao: str = ""
+
+    cancelada: bool = False
+
+    adesao: bool = False
